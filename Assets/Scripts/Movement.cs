06cf02a71184dea999d43076a6ebb95fcc14c131
orodrigzz <oscarrod18503@gmail.com>
2022-10-18 20:39:59 +0200

    Cámara y Modelo y Animaciones Personaje

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private CharacterController controller;
+
+    private Vector3 finalVelocity = Vector3.zero;
+    private float velocityXZ = 5f;
+
+    [SerializeField]
+    Camera camera;
+
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+
+        //Calcular direccion XZ
+        Vector3 direction = Quaternion.Euler(0f, camera.transform.eulerAngles.y, 0f) * new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        direction.Normalize();
+
+        //Calcular velocidad XZ
+        finalVelocity.x = direction.x * velocityXZ;
+        finalVelocity.z = direction.z * velocityXZ;
+
+        controller.Move(finalVelocity * Time.deltaTime);
+    }
+
+}
