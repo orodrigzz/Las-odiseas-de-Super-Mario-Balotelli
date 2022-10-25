a03e4e30bdfb4bafd72b59a0feb9b78a4b5414b5
orodrigzz <oscarrod18503@gmail.com>
2022-10-25 16:39:47 +0200

    25 Octubre -  Camara y Textura personaje fix

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject target;
+
+    [SerializeField]
+    private float targetDistance;
+
+    [SerializeField]
+    private float cameraLerp; //12f
+
+    private float rotationX;
+    private float rotationY;
+    
+    private void LateUpdate()
+    {
+        rotationX += Input.GetAxis("Mouse Y");
+        rotationY += Input.GetAxis("Mouse X");
+
+        rotationX = Mathf.Clamp(rotationX, -10f, 47f);
+
+        transform.eulerAngles = new Vector3(rotationX, rotationY, 0);
+
+        Vector3 finalposition = Vector3.Lerp(transform.position, target.transform.position - transform.forward * targetDistance, cameraLerp * Time.deltaTime);
+
+        RaycastHit hit;
+
+        if(Physics.Linecast(target.transform.position, finalposition, out hit))
+        {
+            finalposition = hit.point;
+        }
+
+        transform.position = finalposition;
+
+    }
+}
